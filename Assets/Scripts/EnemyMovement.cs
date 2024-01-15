43b4264645c50622eb0c58883af88271c9db2ce1
Ethan Smith <xpurelygolden1x@gmail.com>
2024-01-14 22:23:49 -0900

    Enemy Movement
    Basic Enemy Movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    GameObject player;
+    float enemySpeed;
+    Vector3 targetPos;
+
+    void Start()
+    {
+        enemySpeed = 1f;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.name == "Cube")
+        {
+            player = collision.gameObject;
+            Vector3 targetPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            transform.LookAt(targetPos);
+            MoveEnemy();
+        }
+        
+    }
+
+    void MoveEnemy()
+    {
+        transform.position = transform.position + (transform.forward * enemySpeed) * Time.deltaTime;
+    }
+}
