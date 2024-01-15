7c3859837d036416ac28e00081d851d0329af324
Crunchy <jpwill2004@gmail.com>
2024-01-14 23:24:33 -0600

    Uploaded Files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    PlayerInput input;
+    Vector2 movementValue;
+
+
+
+    void Start() {
+        input = GetComponent<PlayerInput>();
+    }
+
+    void Update() {
+        //transform.position = 
+    }
+
+    void handleMovement () {
+    
+    }
+    
+}
