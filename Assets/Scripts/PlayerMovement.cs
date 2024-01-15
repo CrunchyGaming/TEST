9332091a63dd22e56703b0aa4bcb59a439bbd650
Crunchy <jpwill2004@gmail.com>
2024-01-15 00:45:30 -0600

    Fixed Rotation Player Movement
    still need animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [Header("Settings")]
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotationSpeed = 5f;
     Vector2 moveDirection = Vector2.zero;
 
     [SerializeField] PlayerInput playerControls;
@@ -35,12 +37,21 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update() {
+        handleMovement();
+    }
+
+    void handleMovement() {
         moveDirection = move.ReadValue<Vector2>();
 
-        Vector3 newPosition = transform.position + 
+        Vector3 newPosition = transform.position +
             new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed * Time.deltaTime;
 
         transform.position = newPosition;
+
+        if (moveDirection != Vector2.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.y));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     void Fire(InputAction.CallbackContext context) {
