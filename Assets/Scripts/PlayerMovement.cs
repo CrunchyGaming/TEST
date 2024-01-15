dd2c0426bb5a6065ec00667e377a9256eddcf07c
Crunchy <jpwill2004@gmail.com>
2024-01-15 00:39:51 -0600

    Fixed Player Movement
    havent added rotation yet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    PlayerInput input;
-    Vector2 movementValue;
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 moveDirection = Vector2.zero;
+
+    [SerializeField] PlayerInput playerControls;
+    InputAction move;
+    InputAction fire;
 
 
 
-    void Start() {
-        input = GetComponent<PlayerInput>();
+    void Awake() {
+        playerControls = new PlayerInput();
+    }
+
+    void OnEnable() {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    void OnDisable() {
+        move.Disable();
+        fire.Disable();
     }
 
     void Update() {
-        //transform.position = 
+        moveDirection = move.ReadValue<Vector2>();
+
+        Vector3 newPosition = transform.position + 
+            new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed * Time.deltaTime;
+
+        transform.position = newPosition;
     }
 
-    void handleMovement () {
-    
+    void Fire(InputAction.CallbackContext context) {
+        Debug.Log("Fired");
     }
-    
+
 }
