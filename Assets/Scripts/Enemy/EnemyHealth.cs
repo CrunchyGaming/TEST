a51fe158c332952aa71712462f67b9aa3eeeeb0f
Ethan Smith <xpurelygolden1x@gmail.com>
2024-01-17 19:16:25 -0900

    Enemy Flea AI
    -Added enemy will Flea if at or below 20 hp.
    -Enemy will flea to a safe place and heal.
    -Enemy will return to the fight after healing.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,14 +7,28 @@ public class EnemyHealth : MonoBehaviour
 {
 
     [SerializeField] float enemyHealth = 100f;
+    [SerializeField] float timeToHeal = 8f;
 
     public Slider healthSlider;
+
+    EnemyMovement enemyMovement;
   
+
+    void Start()
+    {
+        enemyMovement = GetComponent<EnemyMovement>();
+    }
 
     public void EnemyTakeDamage(float damage)
     {
         enemyHealth -= damage;
         UpdateHeatlhBar();
+
+        if (enemyHealth <= 20)
+        {
+            enemyMovement.Flea();
+            Invoke("HealFleadEnemy", timeToHeal);
+        }
 
         if (enemyHealth <= 0) {
             Die();
@@ -37,5 +51,11 @@ public class EnemyHealth : MonoBehaviour
         healthSlider.value = enemyHealth;
     }
 
+    void HealFleadEnemy()
+    {
+        enemyHealth = 100f;
+        UpdateHeatlhBar();
+        enemyMovement.canTrackPlayer = true;
+    }
 
 }
