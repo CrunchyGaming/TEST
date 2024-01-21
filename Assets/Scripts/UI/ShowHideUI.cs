9a1527f509dffff87bd89d9e9cddb5e8e26fe188
Crunchy <jpwill2004@gmail.com>
2024-01-20 20:59:49 -0600

    Inventory Updates
    - Changed Tab to default open stats
    - Changed i to default open inventory
    - Now have temp placeholders to toggle between the 3 menus

--- Assets/Scripts/UI/ShowHideUI.cs
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class ShowHideUI : MonoBehaviour
 {
 
-    [SerializeField] KeyCode toggleKey = KeyCode.I;
-    [SerializeField] KeyCode toggleKeyAlt = KeyCode.Tab;
+    [SerializeField] KeyCode toggleKeyInventory = KeyCode.I;
+    [SerializeField] KeyCode toggleKeyStats = KeyCode.Tab;
     [SerializeField] GameObject uiContainer = null;
-  
 
-    
+    [Header("Tabs")]
+    [SerializeField] GameObject uiContainerStats = null;
+    [SerializeField] GameObject uiContainerInventory = null;
+    [SerializeField] GameObject uiContainerSkills = null;
+
+
+
     void Start()
     {
         uiContainer.SetActive(false);
     }
 
     void Update() {
-        if (Input.GetKeyDown(toggleKey) || Input.GetKeyDown(toggleKeyAlt))
+        if (Input.GetKeyDown(toggleKeyStats))
         {
             uiContainer.SetActive(!uiContainer.activeSelf);
+            uiContainerStats.SetActive(true);
+            uiContainerInventory.SetActive(false);
+            uiContainerSkills.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(toggleKeyInventory)) {
+            uiContainer.SetActive(!uiContainer.activeSelf);
+            uiContainerStats.SetActive(false);
+            uiContainerInventory.SetActive(true);
+            uiContainerSkills.SetActive(false);
         }
     }
 }
